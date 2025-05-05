032240a83eac429ebc4ee4246de8034e2aeda5b9
albertdaniell <albertagoya@gmail.com>
2025-05-05 11:47:28 +0300

    making changes and fixing images

--- app/components/HomePages/Conclusion.js
+++ app/components/HomePages/Conclusion.js
@@ -1,10 +1,12 @@
+"use client"
 import React from "react";
 import {  Slide } from "react-awesome-reveal";
+import { Typewriter } from "react-simple-typewriter";
 
 function Conclusion() {
   return (
     <div
-      className="relative bg-cover bg-center bg-no-repeat py-24 px-6 md:px-16 text-white"
+      className="relative bg-cover bg-center bg-no-repeat py-52 px-6 md:px-16 text-white"
       style={{
         backgroundImage: "url('/images/conclusion.png')", // Replace with your image path
       }}
@@ -17,8 +19,23 @@ function Conclusion() {
         INVESTING IN GIRLS MEANS INVESTING IN NATIONS
         </h2>
         </Slide>
+        <Typewriter
+            words={[
+              "This is more than training — it’s a mission to lift women and girls out of digital exclusion.",
+              "When one woman is empowered, her whole community benefits.",
+              "Let’s give them the tools to thrive.",
+              "Digital entrepreneurship can be the path that lifts millions out of poverty.",
+              "Together, we can.",
+            ]}
+            loop={3}
+            cursor
+            cursorStyle="_"
+            typeSpeed={30}
+            deleteSpeed={20}
+            delaySpeed={4000}
+          />
         
-        <Slide direction="up">
+        {/* <Slide direction="up">
         <p className="text-lg md:text-md mb-6">
         This is more than training — it’s a mission to lift women and girls out of digital exclusion. When one woman is empowered, her whole community benefits.
         </p>
@@ -28,7 +45,7 @@ function Conclusion() {
         Let’s give them the tools to thrive. Digital entrepreneurship can be the path that lifts millions out of poverty.
         </p>
         <p className="text-lg md:text-md mt-6 italic">Together, we can.</p>
-        </Slide>
+        </Slide> */}
 
        
        
